17bde9c0d9b52ea72e62c73b777f87a4af8b69ce
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-12-25 18:31:21 +0600

    search feature implemented

--- README.md
+++ README.md
@@ -3,31 +3,12 @@ live link: (https://assignment-11-3e9b0.web.app)
 
 
 
-## 
-Brief description of your project.
-
+# Description:
+It's a simple react library website with some functionality. Here are the five features of my website:
 ## Features
-- Feature 1: Describe the first key feature of your website.
-- Feature 2: Describe the second key feature.
-- Feature 3: ...
-- Feature 4: ...
-- Feature 5: ...
-
-## Getting Started
-Instructions on how to get a copy of the project and run it on a local machine.
-
-### Prerequisites
-List any software or tools that users need to have installed before running your project.
-
-### Installation
-Step-by-step instructions on how to install and set up your project.
-
-## Usage
-Instructions on how to use your website and any additional information users need.
-
-## Contributing
-Explain how others can contribute to your project. Include guidelines for submitting pull requests.
-
-## License
-Specify the license for your project.
+- Feature 1: This website firebase based authentication user can login using google or email and password
+- Feature 2: There are different routes in website for user and admin of the site. user can see the books available and borrow book
+- Feature 3: There is a add book route , new book can be added in the library using this route
+- Feature 4: There is a update route user can update information about any book using update route
+- Feature 5: The website provide responsiveness for different devices like laptop, mobile and tablets,and there is a daisy ui based dark mode
 
--- src/page/AllBooks/AllBooks.jsx
+++ src/page/AllBooks/AllBooks.jsx
@@ -1,25 +1,40 @@
 import BooksCard from "./BooksCard";
-import  useAllBooks from "../../hooks/allBooks";
- "../../hooks/allBooks";
+import useAllBooks from "../../hooks/allBooks";
+import { useState } from "react";
+import axios from "axios";
+"../../hooks/allBooks";
 
 const AllBooks = () => {
-    const {data,isLoading,refetch}=useAllBooks()
-    if(isLoading){
-        return <h2>loading</h2>
-    }
-    if(!isLoading){
+    const [query, setQuery] = useState('')
+    const { data, refetch } = useAllBooks()
+    const [displayBooks,setDisplayBooks] = useState(data)
+   
+        const handleSearch = async () => {
+            const response = await axios.get(`http://localhost:5000/books/search?name=${query}`);
+            console.log(response)
+            setDisplayBooks(response.data);
 
-        return (
-            <div>
-            <h3 className="my-8 text-2xl font-bold text-center">Total Books:{data?.length}</h3>
+        };
+    return (
+        <div>
+            <h3 className="my-8 text-2xl font-bold text-center">Total Books:{displayBooks?.length}</h3>
+            <div className='lg:flex items-center flex-row justify-center mb-8 w-full'>
+                <input type="text"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder='search book here' className='border-black pl-4 w-2/4  p-2 bg-base-300 flex' name="" id="" />
+                <button onClick={handleSearch} className='text-xl font-semibold bg-black text-white px-3 py-2  rounded-lg '>Search</button>
+
+            </div>
+
             <div className="grid grid-cols-1 lg:grid-cols-4  md:grid-cols-3  gap-6 p2">
                 {
-                    data?.map(book => <BooksCard key={book._id} refetch={refetch} book= {book}></BooksCard> )
+                    displayBooks?.map(book => <BooksCard key={book._id} refetch={refetch} book={book}></BooksCard>)
                 }
             </div>
         </div>
     );
 }
-};
+
 
 export default AllBooks;
